f1e9dc28931c44a7fa081fdb1456dd1e13b15fad
NhatTruong <75980050+docnhiho@users.noreply.github.com>
2024-01-01 17:13:53 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -15,11 +15,18 @@ function App() {
 
 
 
-      <p className='text-[50px] leading-[10px]'>Hello 10px</p>
+      {/* <p className='text-[50px] leading-[10px]'>Hello 10px</p>
       <div className='w-20 h-20 m-4 bg-slate-600'></div>
       <div className='size-6 bg-red-500' ></div>
       <div className='w-[20px] m-[1500px]'>abs</div>
-      <div className="rounded-lg bg-red-500 size-20"></div>
+      <div className="rounded-lg bg-red-500 size-20"></div> */}
+
+      {/* <div class="rem:text-[20px]">Hello World!</div>  */}
+
+
+      <div className="rem:text-[20px]">Hello World!</div> {/* convert to 1rem */}
+      <div className="rem:m-[100px]">Hello World</div> {/* convert to 5rem */}
+
 
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,48 @@ module.exports = {
   theme: {
     extend: {},
   },
-  
-  plugins: [
 
-  ],
-  
+  // plugins: [
+  //   function ({ addUtilities, theme, variants }) {
+  //     const baseFontSize = theme('fontSize.base', '1rem'); // Có thể thay đổi giá trị mặc định tại đây
+  //     const utilities = {};
+
+  //     for (let i = 0; i <= 100; i++) {
+  //       const valueInRem = `${i / 20}rem`; // Chuyển đổi từ px sang rem
+  //       utilities[`.text-${i}`] = { fontSize: `${i}px` };
+  //       utilities[`.text-${i}px-rem`] = { fontSize: valueInRem };
+  //     }
+
+  //     addUtilities(utilities, variants('fontSize'));
+  //   },
+  // ],
+
+  // plugins: [
+  //   function ({ addUtilities, variants }) {
+  //     const utilities = {};
+
+  //     // Hàm chuyển đổi giá trị từ px sang rem
+  //     const pxToRem = (value) => `${parseFloat(value) / 16}rem`;
+
+  //     // Tự động tạo utility classes cho margin, padding và font size
+  //     ['margin', 'padding', 'fontSize'].forEach((property) => {
+  //       for (let i = 0; i <= 100; i++) {
+  //         const className = `.${property}-${i}`;
+  //         const valueInPx = `${i * 4}px`; // Giá trị mặc định trong px
+  //         const valueInRem = pxToRem(valueInPx); // Chuyển đổi giá trị sang rem
+  //         utilities[`${className}-rem`] = { [property]: valueInRem };
+  //       }
+  //     });
+
+  //     addUtilities(utilities, variants(['responsive', 'hover']));
+  //   },
+  // ],
+  plugins:  [
+    require('tailwindcss-convert-px-to-rem')
+    
+    
+  ]
+
 
 }
 
