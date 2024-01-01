57051a4042a15fdbe6c1d3c1bca9f58bc8b2fc22
NhatTruong <75980050+docnhiho@users.noreply.github.com>
2024-01-01 12:09:14 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="">
-      <p className='text-2xl'>hello</p>
-      <p className='text-[50px] leading-8 '>Hello 10px</p>
-      <div className='w-20 h-20 bg-slate-600'></div>
-      <div className='w-[20px] m-[100px]'>abs</div>
+
+
+
+
+
+      <p className='text-base w-10 h-20'>Hello World</p>
+
+
+
+
+      <p className='text-[50px] leading-[10px]'>Hello 10px</p>
+      <div className='w-20 h-20 m-4 bg-slate-600'></div>
+      <div className='size-6 bg-red-500' ></div>
+      <div className='w-[20px] m-[1500px]'>abs</div>
+      <div className="rounded-lg bg-red-500 size-20"></div>
+
     </div>
   );
 }
+
 
 export default App;
