5dda81cb40791d0f387669dfdbc2ffca9cbf4c77
NhatTruong <75980050+docnhiho@users.noreply.github.com>
2024-01-01 09:32:42 +0700

    pxtorem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="">
-      <p className='text-xl'>hello</p>
+      <p className='text-2xl'>hello</p>
+      <p className='text-[50px] leading-8 '>Hello 10px</p>
+      <div className='w-20 h-20 bg-slate-600'></div>
+      <div className='w-[20px] m-[100px]'>abs</div>
     </div>
   );
 }
